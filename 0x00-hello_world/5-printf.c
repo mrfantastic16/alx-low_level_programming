07a9c3ea4049050fc8c47f94c884e83df0296fd8
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-17 17:26:51 +0100

    prints str with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a str to stdout using printf
+ * Return: returns 0 on succes
+*/
+
+int main(void)
+{
+    printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+    return (0);
+}
