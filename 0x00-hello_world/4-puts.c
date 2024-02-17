8d7fc78379ba2017bdad995765949d2dc96690b3
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-17 17:21:39 +0100

    prints a string with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a given str to stdout
+ * Return: On succes, 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
