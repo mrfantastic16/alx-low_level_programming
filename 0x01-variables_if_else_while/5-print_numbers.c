f44d73b3e291f4f075ebfd2399b706637be34f27
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-19 10:22:27 +0100

    betty coding style and docs, corrected

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,12 +3,13 @@
 /**
  * main - prints all single digits of base 10
  *
- * Returns: 0 on success
+ * Return: 0 on success
  */
 
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 		printf("%d", i);
 	printf("\n");
