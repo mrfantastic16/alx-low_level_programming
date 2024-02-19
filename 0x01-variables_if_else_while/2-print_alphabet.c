fb19391e4805a537018910d35eca82277a30a77d
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-19 09:55:58 +0100

    prints ascii lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase followed by newline
+ *
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	char ascii;
+
+	for (ascii = 'a'; ascii <= 'z'; ascii++)
+		putchar(ascii);
+	putchar('\n');
+}
