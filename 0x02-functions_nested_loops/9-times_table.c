b5c2d434aa89a29934713ae11ba8b5c84b2a3ae0
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-20 17:58:00 +0100

    prints times table from 0 tp 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 times table
+ */
+
+void times_table(void)
+{
+	int i, j, n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+
+			if (n == 0)
+			{
+				_putchar(n + '0');
+			}
+			else if (n < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(n + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
