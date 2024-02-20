d573231bdd16b50cf9d8dc73c26fe6205f8dd845
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-20 15:16:04 +0100

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar" with a newline using _putchar
+ *
+ * Return: 0 :)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
