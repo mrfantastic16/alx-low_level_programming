e8d6e6f4982e2e79631985b9aedad474a9b2532c
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-20 15:36:47 +0100

    prints lowercase alphabets x10 times in a newline

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets x10 in newlines
+ *
+ * Return: its a void function :\
+ */
+
+void print_alphabet_x10(void)
+{
+	char al;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (al = 'a'; al <= 'z'; al++)
+			_putchar(al);
+		_putchar('\n');
+	}
+}
