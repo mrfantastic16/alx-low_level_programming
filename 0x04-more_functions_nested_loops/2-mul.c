a19fff94101db94d438c64685582c124f1cd4b17
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-22 17:46:51 +0100

    returns multilplication of two of its parameters

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - returns the multilplication of two number
+ * @a: fisrt parameter
+ * @b: second parameter
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
