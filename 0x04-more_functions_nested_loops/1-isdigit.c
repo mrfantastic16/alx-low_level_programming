5c97311fe6176e4f79d436cefb4b31188d673bbf
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-22 17:38:50 +0100

    checks if a character is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: int to be checked
+ * Return: 1 if c is digit else 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
