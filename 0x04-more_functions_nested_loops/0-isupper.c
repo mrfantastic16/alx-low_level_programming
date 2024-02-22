900cdf62367e07fa4145a124d1da2b9e6474d266
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-22 17:15:57 +0100

    checks if a char is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1 if inp is upper else 0
+ * @c: character to be checked
+ * Return: 1 if c is upper else 0
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
