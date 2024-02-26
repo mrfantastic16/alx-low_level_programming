5dea4aba10016a87ae8d506515931745755b4dbc
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-26 23:31:33 +0100

    prints second part of halfed str

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,33 +1,18 @@
 #include "main.h"
-
 /**
- * _strlen - returns length of a str
- * @s: string whose length is needed
- *
- * Return: length of @s
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * puts_half - prints half of a str to stdout
- * @str: str whose halfed version is needed
- *
- * Return: Nothing
- */
-
+* puts_half - print second half of a string
+* @str: char array string type
+* Description: If odd number of chars, print (length - 1) / 2
+*/
 void puts_half(char *str)
 {
-	int ln = _strlen(str);
+	int i;
 
-	for (ln / 2; str[ln] != '\0'; ln++)
-		_putchar(str[ln]);
+	for (i = 0; str[i] != '\0'; i++)
+		i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
