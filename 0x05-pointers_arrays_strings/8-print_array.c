4c8c94f1919822b3e5f311172e8530db88f1a546
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-02-26 23:43:03 +0100

    prints specfied amnt of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints specified number of arrays
+ * @a: array to be printed
+ * @n: spefied number
+ *
+ * Return: Nothing!
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
