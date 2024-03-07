7142bf4d1e9629de9436aaa3958ec6518860b182
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-03-07 13:15:14 +0100

    func concatenates two strs

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+/**
+ * _strcat - appends src to dest
+ * @src: source strs to appended
+ * @dest: destination str in which src would be appended to
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
