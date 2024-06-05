9bb2307b13347464896079d19c062f2f6d4e9c42
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-06-05 18:40:26 +0100

    get a pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+/**
+ * _strchr - returns the address of a specified char it has to find
+ * in a string
+ * @s: string to be searched
+ * @c: char to find in the string
+ *
+ * Return: a pointer of c if found else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+
+	for (i = 0; i < strlen(s); i++)
+		if (s[i] == c)
+			return (&s[i]);
+	return (NULL);
+}
