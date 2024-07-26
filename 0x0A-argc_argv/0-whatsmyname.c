de8f514d12e38fba4596532cbf584eaabd492327
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-07-26 11:42:41 +0100

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: arguments counts from the command line
+ * @argv: arguement received as strings from the command line.
+ *
+ * Return: Always 1
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
