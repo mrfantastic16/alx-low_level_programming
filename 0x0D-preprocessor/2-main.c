e714e9817366821faab7c8fdc02378d287ae7adf
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-07-30 12:56:21 +0100

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * prints out the file name as it would be compiled from
+ * main - prints out the file name as it would be compiled from
  * Return: Always 0.
  */
 int main(void)
