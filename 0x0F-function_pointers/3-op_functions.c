1c973c86fb561ccfe75ed143d071cbaa615877c1
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-08-05 12:32:30 +0100

    calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+/**
+ * op_add - calculates the sum of the two numbers received
+ * @a: num1
+ * @b: num2
+ *
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates the difference the two nums
+ * @a: num1
+ * @b: num2
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the product of two nums
+ * @a: num1
+ * @b: num2
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates the division of one num with another
+ * @a: quotient
+ * @b: divisor
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns num mod num
+ * @a: num1
+ * @b: num2
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
