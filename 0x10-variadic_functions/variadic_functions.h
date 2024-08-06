a579c4aa66c3d125caa8a1c57a2d9e155c46e0f9
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-08-06 13:53:23 +0100

    h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,24 @@
 #ifndef VAR_H
 #define VAR_H
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct formatSpec - new type bundled with the symbol and a function pointer
+ * which would be used to call the function corresponding to symbol
+ *
+ * @sy: symbol
+ * @func: function pointer to actuall function correspponding
+ * to @sy
+ */
+typedef struct formatSpec
+{
+	char *sy;
+	void (*func)(va_list args);
+} formatSpec_t;
 
 #endif
