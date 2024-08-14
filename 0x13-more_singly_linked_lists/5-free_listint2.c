d9b5458ef9a0183046e9a1f107721ae924c21300
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2024-08-14 15:51:37 +0100

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,15 @@
 #include "lists.h"
 
+/**
+ * free_listint2 - fjfjhdhjd
+ * @head: head
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
+
+	if (!head)
+		return;
 
 	while (*head)
 	{
